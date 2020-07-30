6fb9993a7ac166d13d988a15113847f9f2f27e8b
Priest Sabo Ombugadu <sabopriest@gmail.com>
2020-07-30 22:00:07 +0100

    feat: Image asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,36 @@
+import 'dart:math' as Math;
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.teal),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("assets/images/priest.jpg"),
+            ),
+            Text(
+              "Priest S.O.",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
