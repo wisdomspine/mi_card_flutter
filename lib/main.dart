00f830e0e20260708c592356841020323d0a074c
Priest Sabo Ombugadu <sabopriest@gmail.com>
2020-07-31 21:17:13 +0100

    feat: Done!

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.teal),
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage("assets/images/priest.jpg"),
-            ),
-            Text(
-              "Priest S.O.",
-              style: TextStyle(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("assets/images/priest.jpg"),
+              ),
+              Text(
+                "Priest S.O.",
+                style: TextStyle(
                   color: Colors.white,
                   fontSize: 40.0,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
-        )),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "SOFTWARE DEVELOPER",
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  fontFamily: "Source Sans Pro",
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade300,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 25.0,
+                  vertical: 10.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 20.0,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+234 816 060 6990",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: "Source Sans Pro",
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 20.0,
+                    ),
+                    title: Text(
+                      "sabopriest@gmail.com",
+                      style: TextStyle(
+                        fontFamily: "Source Sans Pro",
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
